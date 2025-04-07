c95205cf0791f65b541b66ca095558eab8b62f3f
William Riser <riser.w@northeastern.edu>
2025-04-07 16:20:24 -0400

    map centered on boston and no labels

--- city-story-frontend/src/App.jsx
+++ city-story-frontend/src/App.jsx
@@ -1,22 +1,38 @@
 import './App.css'
-import {APIProvider, Map} from '@vis.gl/react-google-maps';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
-const key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+const key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+const mapOptions = {
+  styles: [
+    {
+      featureType: 'poi',
+      elementType: 'labels',
+      stylers: [{ visibility: 'off' }]
+    },
+    {
+      featureType: 'transit',
+      elementType: 'labels',
+      stylers: [{ visibility: 'off' }]
+    }
+  ]
+};
 
 function App() {
   return (
     <>
       <APIProvider apiKey={key}>
-    <Map
-      style={{width: '100vw', height: '100vh'}}
-      defaultCenter={{lat: 22.54992, lng: 0}}
-      defaultZoom={3}
-      gestureHandling={'greedy'}
-      disableDefaultUI={true}
-    />
-  </APIProvider>
+        <Map
+          style={{ width: '100vw', height: '100vh' }}
+          center={{ lat: 42.3601, lng: -71.0589 }}
+          defaultZoom={14.5}
+          gestureHandling={'greedy'}
+          disableDefaultUI={true}
+          options={mapOptions}
+        />
+      </APIProvider>
     </>
   )
 }
 
-export default App
+export default App;
