32963a2e1463eb5b0d1ba9171a3211ebae61aea7
William Riser <riser.w@northeastern.edu>
2025-04-07 16:02:06 -0400

    Add google maps

--- city-story-frontend/src/App.jsx
+++ city-story-frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import {APIProvider, Map} from '@vis.gl/react-google-maps';
+
+const key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Test</h1>
-      </div>
+      <APIProvider apiKey={key}>
+    <Map
+      style={{width: '100vw', height: '100vh'}}
+      defaultCenter={{lat: 22.54992, lng: 0}}
+      defaultZoom={3}
+      gestureHandling={'greedy'}
+      disableDefaultUI={true}
+    />
+  </APIProvider>
     </>
   )
 }
