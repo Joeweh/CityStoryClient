25b1bcf0f635b0b0d5d3d092fd265428dd435511
William Riser <riser.w@northeastern.edu>
2025-04-09 15:28:18 -0400

    Add placeholder marker

--- city-story-frontend/src/App.jsx
+++ city-story-frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import { 
+  AdvancedMarker,
+  APIProvider,
+  InfoWindow,
+  Map,
+  Marker,
+  Pin } from '@vis.gl/react-google-maps';
+import { MarkerWithInfowindow } from './components/MarkerWithInfoWindow';
 
 const key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-
 const mapOptions = {
   styles: [
     {
@@ -18,19 +24,39 @@ const mapOptions = {
   ]
 };
 
+const getResponse = async () => {
+  try {
+    const response = await fetch("https://city-story-server-1dab1cdfb3b7.herokuapp.com/ping");
+    const data = await response.json();
+    console.log(data);}
+  catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+getResponse();
+
+
+
 function App() {
   return (
     <>
       <APIProvider apiKey={key}>
         <Map
+          mapId={'6f0b5d4f2c4c0e9c'}
           style={{ width: '100vw', height: '100vh' }}
           center={{ lat: 42.3601, lng: -71.0589 }}
           defaultZoom={14.5}
           gestureHandling={'greedy'}
           disableDefaultUI={true}
           options={mapOptions}
-        />
+        >
+
+      <MarkerWithInfowindow />
+
+          </Map>
       </APIProvider>
+      
     </>
   )
 }
